52a0843cf86bd3b6cd4aec775e1c3582312090ba
Your Name <you@example.com>
2024-03-15 11:45:34 +0300

    2.2.2

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,20 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import web.dao.CarDao;
+import web.model.Car;
+
+import java.util.List;
+
+@Component
+public class CarServiceImpl implements CarService {
+
+    @Autowired
+    private CarDao carDao;
+
+    @Override
+    public List<Car> getCar(int count) {
+        return carDao.getCar(count);
+    }
+}
